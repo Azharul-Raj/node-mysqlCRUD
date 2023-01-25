a33256a375c6f9c542ab1b67e898f0be1f3261d4
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-25 12:56:27 +0600

    add user function created

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import mysql2 from 'mysql2';
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 const port = process.env.PORT || 3001
 
 app.listen(port, () => {
@@ -10,4 +13,36 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
     res.send("WOW!! Server is up and running")
+})
+
+const db = mysql2.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"araj",
+    database:"user"
+})
+
+app.get("/users", (req, res) => {
+    const q = "SELECT * FROM user.users";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+});
+
+app.post('/add_user', (req, res) => {
+    // console.log(req.body);
+    const q = "INSERT INTO users (`email`,`password`,`type`,`active`) VALUES (?)"
+    const { email, password, type, active } = req.body;
+    console.log(email,password)
+    const values = [
+        email,
+        password,
+        type,
+        active
+    ]
+    db.query(q, [values], (err, data) => {
+        if (err) res.send(err);
+        return res.json("Data inserted successfully")
+    })
 })
