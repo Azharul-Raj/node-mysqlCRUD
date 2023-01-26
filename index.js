d1ecef54bfe0c3a2539d60267ea1ca272c21c8bc
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-26 08:22:06 +0600

    file uploaded part done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mysql2 from 'mysql2';
+import multer from 'multer';
 
 const app = express();
 app.use(cors());
@@ -11,26 +12,55 @@ app.listen(port, () => {
     console.log(`Server is running at 3001`)
 })
 
-app.get("/", (req, res) => {
-    res.send("WOW!! Server is up and running")
-})
-
+/*-------------------------
+DATABASE CONNECTION
+--------------------------*/
 const db = mysql2.createConnection({
     host: "localhost",
     user: "root",
     password:"araj",
     database:"user"
 })
-// procedure 
 
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connected to the database!");
-  });
+});
+  
+app.get("/", (req, res) => {
+    res.send("WOW!! Server is up and running")
+})
+
+/*-------------------------------
+UPLOAD IMAGE TO THE SERVER START
+--------------------------------*/
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+     cb(null,'upload_images')
+    },
+    filename: function (req, file, cb) {
+        cb(null,file.originalname)
+    }
+})
+
+
+const upload = multer({ storage: storage }).single('image');
+app.post('/upload', (req, res) => {
+    // console.log(req);
+    upload(req, res, (err) => {
+        if (err) return res.json(err);
+        res.send(`Image uploaded successfully`)
+    })
+})
+/*---------------------------
+FILE UPLOAD TO THE SERVER END
+-----------------------------*/
   
   
   /* Once table got created you can remove the code and do the following operation below*/
-  /* Table and procedure created below*/
+  /*---------------------------------
+  TABLE AND PROCEDURE CREATED BELOW
+   --------------------------------*/
   db.query(
     "CREATE TABLE users (ID INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) CHARACTER SET 'utf8mb4' NOT NULL, password VARCHAR(255) CHARACTER SET 'utf8mb4' NOT NULL, type VARCHAR(255) CHARACTER SET 'utf8mb4' NOT NULL, active TINYINT default 1, PRIMARY KEY (ID))",
     function(err, result) {
@@ -55,6 +85,9 @@ db.connect(function(err) {
   });
 
 /* .......................................................*/
+/*-------------------------
+GETTING USER METHOD
+--------------------------*/
 app.get("/users", (req, res) => {
     const q = "SELECT * FROM user.users";
     db.query(q, (err, data) => {
@@ -62,7 +95,9 @@ app.get("/users", (req, res) => {
         return res.json(data);
     })
 });
-
+/*-------------------------
+ADDING USER METHOD
+--------------------------*/
 app.post('/add_user', async(req, res) => {
     // console.log(req.body);
     const q = "INSERT INTO users (`email`,`password`,`type`,`active`) VALUES (?)"
@@ -79,7 +114,9 @@ app.post('/add_user', async(req, res) => {
         return res.json("Data inserted successfully")
     })
 })
-// update user method
+/*-------------------------
+UPDATE USER METHOD
+--------------------------*/
 app.put("/user_update/:id", (req, res) => {
     const { id } = req.params;
     const { email, password, type, active } = req.body;
@@ -91,7 +128,9 @@ app.put("/user_update/:id", (req, res) => {
     })
 })
 
-// delete a user
+/*-------------------------
+DELETE USER METHOD
+--------------------------*/
 app.delete('/user/:id', (req, res) => {
     const { id } = req.params;
     const q = "DELETE FROM users WHERE id = ?"
